746398507132fa30a4e7129455c870e03ddddb84
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-01-08 20:29:42 +0300

    custom styles, assets and App templete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { Hero, CustomerReviews, Services, SpecialOffer, SuperQuality, Subscribe, PopularProducts, Footer } from './sections/index'
+import Nav from './Components/Nav';
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 p-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero.jsx'
+import SuperQuality from './SuperQuality.jsx'
+import SpecialOffer from './SpecialOffer.jsx'
+import PopularProducts from './PopularProducts.jsx'
+import Services from './Services.jsx'
+import Subscribe from './Subscribe.jsx'
+import CustomerReviews from './CustomerReviews.jsx'
+import Footer from './Footer.jsx'
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer
+};
