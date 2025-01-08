b3453a4e0a31acd6a905e892a51ac2664b568103
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-01-09 00:29:15 +0300

    hero section

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from '../assets/images/index.js'
 import { hamburger } from '../assets/icons'
+import { navLinks } from '../Constants/index.js'
 
 const Nav = () => {
   return (
@@ -13,8 +14,24 @@ const Nav = () => {
                 height={29} />
             </a>
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a href={item.href}
+                           className='font-montserrat leading-normal text-lg text-slate-gray'
+                           >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
             </ul>
+            <div className='hidden max-lg:block'>
+                <img 
+                    src={hamburger} 
+                    alt="Toggle Menu"
+                    width={25}
+                    height={25} 
+                    />
+            </div>
         </nav> 
     </header>
   )
